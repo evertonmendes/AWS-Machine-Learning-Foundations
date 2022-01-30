2d60a049fb6f71a03ad1ba2c132b486e46012efc
evertonmendes <63795580+evertonmendes@users.noreply.github.com>
2022-01-30 01:24:44 -0300

    Add files via upload

---
+++ Second/SecondSection.py
@@ -0,0 +1,112 @@
+#Founder of Udacity is Sebastian thrun
+"examples"
+#we can improve work safety 
+#or speeding up drug discovery, 
+#detection of breast cancer,
+#It allows for more accurate and rapid translation of the text into hundreds of languages,
+#and so on
+"machine learning"
+#we can define this area of AI in three main subjects. 
+'''
+-supervised learning
+    training samples that have label as target
+-unsupervised learning
+    training a dataset to find some pattern or distribution
+-reinforcement learning
+    find a model by defining a feature to maximize 
+'''
+"Components of Machine Learnig"
+#nearly all tasks solved with ML involves three primary components.
+'''
+machine learning model - a block of code used to solve different problems
+model training algorithm -  determine what changes need to be made, makes small changes to the model
+model inference algorithm- using a trained model to generate predictions
+'''
+
+
+"Usual steps in ML" "(begin)"
+'''
+- define the problem
+- build the dataset
+- train the model
+- evaluate the  model
+- inference(Use the model)
+'''
+
+
+#step One: Define he problem
+'''
+    Define a very specific task.
+    - "How do I increase sales?" It's a valid question,
+    but it's the opposite of a very specific task
+    - “Does adding a $1.00 charge for sprinkles on a hot
+    fudge sundae increase the sales of hot fudge sundaes?”
+    This is a valid question
+
+    Identify the machine learning task we might use to solve this problem.
+    - This helps you better understand the data you need for a project.
+
+    What is a Machine Learning Task?
+        Supervised learning
+        Unsupervised learning
+
+                                    - Categorical(Classification)
+            -Supervised Learning    - Continuous Label(Regression)
+    ML Task                         - ...
+            
+            -Unsupervised Learning  - Clustering
+                                    - ...
+'''
+
+#step Two: Build a Dataset
+'''
+    Data collection
+    -Does the data you've collected match the machine learning task and
+    problem you have defined?
+
+    Data inspection
+    -Outliers
+    -Missing or incomplete values
+    -Data that needs to be transformed or preprocessed so it's in the correct
+    format to be used by your model
+
+    Summary statistics
+    -mean, standard deviation, inner-quartile range, ...
+
+    Data visualiization
+    -You can use data visualization to see outliers and trends in your data and
+    to help stakeholders understand your data
+
+'''
+
+#step Three: model training
+'''
+    Spliting your Dataset
+    -The first step in model training is to randomly split the dataset
+    -Many developers estimate about 80% in training and the rest, 20%, to make evaluations
+
+    Model Training Terminology
+    -model parameters: configurations that can be update to change how the model behaves, during the training
+    -loss function: distance from your prediction to the target value
+    -hyperparameters: parameters which are not changed during the training, but can affect the accuracy of the algorithm
+
+    Extended Learning
+    -Linear models, tree-based models, deep learning
+    -usual deep learnings: FFNN, CNN, RNN/LSTM, Transformer
+
+'''
+
+#step Four: model evaluation
+'''
+    -Log Loss: calculate how uncertain your model is about the predictions it is generating
+    -Model Accuracy is the fraction of predictions a model gets right
+    -Mean absolute error (MAE), Root mean square error (RMSE), Coefficient of determination or R-squared (R^2
+'''
+
+#step five: model inference
+'''
+'''
+"Usual steps in ML" "(end)"
+
+
+
